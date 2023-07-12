1a35e5d4d2a8d1ba9d03efca2e48f21773e24b63
aishwaryatk <aishwarytk@gmail.com>
2023-07-12 18:47:40 +0530

    urban_jungle

---
+++ customer/views.py
@@ -0,0 +1,179 @@
+import json
+from django.shortcuts import render, redirect
+from django.views import View
+from django.db.models import Q
+from django.core.mail import send_mail
+from .models import MenuItem, Category, OrderModel
+from django.conf import settings
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/index.html')
+
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/about.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+
+        appetizers = MenuItem.objects.filter(
+            category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+
+
+        return render(request, 'customer/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            state=state,
+            zip_code=zip_code
+        )
+        order.items.add(*item_ids)
+
+
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return redirect('order-confirmation', pk=order.pk)
+
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price,
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        data = json.loads(request.body)
+
+        if data['isPaid']:
+            order = OrderModel.objects.get(pk=pk)
+            order.is_paid = True
+            order.save()
+
+        return redirect('payment-confirmation')
+
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')
+
+
+class Menu(View):
+    def get(self, request, *args, **kwargs):
+        menu_items = MenuItem.objects.all()
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
+
+
+class MenuSearch(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get("q")
+
+        menu_items = MenuItem.objects.filter(
+            Q(name__icontains=query) |
+            Q(price__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
+
+
+class Route(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            "google_api_key": settings.GOOGLE_API_KEY,
+            "base_counrty": settings.BASE_COUNTRY}
+        return render(request, 'customer/route.html', context)
+
+
+# class Map(View):
+#     def get(self, reque
+#         if lat_a and lat_b and lat_c and lat_d:
+# 		directions = Directions(st):
+#         lat_a = request.GET.get("lat_a", None)
+# 	    long_a = request.GET.get("long_a", None)
+#         lat_b = request.GET.get("lat_b", None)
+# 	    long_b = request.GET.get("long_b", None)
+
+# 			lat_a= lat_a,
+# 			long_a=long_a,
+# 			lat_b = lat_b,
+# 			long_b=long_b,
